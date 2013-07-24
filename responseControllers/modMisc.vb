a87ade12b50d2e6dc662e0b412132d985388070b
xeon927 <aaronmoorley192@gmail.com>
2013-07-24 11:48:56 +1000

    Updated hugging to trigger no matter what order words are in (or if there are spaces between)
    I noticed a bit of an issue with this when on IRC with a few other bots. Using { /me hugs [botname] [secondBotname] } caused IRCBot.v2 to trigger only if it was first. This should fix that issue.

--- responseControllers/modMisc.vb
+++ responseControllers/modMisc.vb
@@ -1,9 +1,9 @@
 ﻿Module modMisc
     Sub Check(message As String)
         If InStr(getMessage(message), String.Format("Goodnight, {0}", nickname)) Then mscSleep(getNickname(message), getChannel(message))
-        If InStr(getMessage(message), String.Format("hugs {0}", nickname)) Then mscHug(getNickname(message), getChannel(message))
-        If InStr(getMessage(message), String.Format("cuddles {0}", nickname)) Then mscHug(getNickname(message), getChannel(message))
-        If InStr(getMessage(message), String.Format("snuggles {0}", nickname)) Then mscHug(getNickname(message), getChannel(message))
+        If InStr(getMessage(message), "hugs") And InStr(getMessage(message), nickname) Then mscHug(getNickname(message), getChannel(message))
+        If InStr(getMessage(message), "cuddles") And InStr(getMessage(message), nickname) Then mscHug(getNickname(message), getChannel(message))
+        If InStr(getMessage(message), "snuggles") And InStr(getMessage(message), nickname) Then mscHug(getNickname(message), getChannel(message))
         If InStr(getMessage(message), "<3") Then
             If InStr(getMessage(message), nickname) Then
                 mscLove(getNickname(message), getChannel(message))
