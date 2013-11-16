300dc61a63aef9cf433a1557468acb7f53860bfc
xeon927 <aaronmoorley192@gmail.com>
2013-11-17 04:43:26 +1100

    HttpUtility.HtmlEncode() is crap. Changed to Uri.EscapeDataString()

--- responseControllers/modFantasy.vb
+++ responseControllers/modFantasy.vb
@@ -1,7 +1,6 @@
 ﻿Imports System.Text.RegularExpressions
 Imports System.Diagnostics
 Imports System.IO
-Imports System.Web 'Needed for HTML-Escaping of WolframAlpha queries
 Module modFantasy
     Sub Check(message As String)
         If getMessage(message).Substring(0, 1) = "!" Then
@@ -166,7 +165,9 @@ Module modFantasy
     Sub fantAlpha(nick As String, chan As String, message As String)
         Try
             Dim query As String = Regex.Match(message, "!wa\ (?<query>.+?)$", RegexOptions.IgnoreCase).Result("${query}")
-            Dim xDoc As XDocument = XDocument.Load(String.Format("http://api.wolframalpha.com/v2/query?format=plaintext&appid={0}&input={1}", waAppID, HttpUtility.HtmlEncode(query)))
+            Console.WriteLine(query)
+            Console.WriteLine(Uri.EscapeDataString(query))
+            Dim xDoc As XDocument = XDocument.Load(String.Format("http://api.wolframalpha.com/v2/query?format=plaintext&appid={0}&input={1}", waAppID, Uri.EscapeDataString(query)))
             If xDoc.Element("queryresult").Attribute("success").Value = "true" Then
                 For Each item As XElement In xDoc.Descendants("queryresult").Elements("pod")
                     For Each podAttribute As XAttribute In item.Attributes
