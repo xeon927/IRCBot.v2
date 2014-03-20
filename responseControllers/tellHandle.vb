9a5eedaf52683444921869466516b68a3199d2e0
xeon927 <aaronmoorley192@gmail.com>
2014-03-20 16:38:08 +1100

    Added DateTime to !tell
    Existing databases will need to be reformatted.
    New syntax:
    destNick|#channel|sendDateTime|message

--- responseControllers/tellHandle.vb
+++ responseControllers/tellHandle.vb
@@ -14,9 +14,9 @@ Module tellHandle
         If File.Exists(tellfilePath) Then
             waitingTells = File.ReadAllLines(tellfilePath)
             For i As Integer = 1 To waitingTells.Length
-                Dim tempArray(2) As String
-                tempArray = waitingTells(i - 1).Split(New Char() {"|"}, 3)
-                waitingTellsList.Add({tempArray(0), tempArray(1), tempArray(2)})
+                Dim tempArray(3) As String
+                tempArray = waitingTells(i - 1).Split(New Char() {"|"}, 4)
+                waitingTellsList.Add({tempArray(0), tempArray(1), tempArray(2), tempArray(3)})
             Next
         Else
             waitingTells = {}
@@ -26,11 +26,11 @@ Module tellHandle
         'Delete file and rewrite
         If File.Exists(tellfilePath) Then File.Delete(tellfilePath)
         For i As Integer = 0 To (waitingTellsList.Count - 1)
-            File.AppendAllLines(tellfilePath, {String.Format("{0}|{1}|{2}", waitingTellsList(i)(0), waitingTellsList(i)(1), waitingTellsList(i)(2))})
+            File.AppendAllLines(tellfilePath, {String.Format("{0}|{1}|{2}|{3}", waitingTellsList(i)(0), waitingTellsList(i)(1), waitingTellsList(i)(2), waitingTellsList(i)(3))})
         Next
     End Sub
     Sub Add(fromNick As String, fromChan As String, destUser As String, message As String)
-        waitingTellsList.Add({destUser, fromChan, String.Format("<{0}> {1}", fromNick, message)})
+        waitingTellsList.Add({destUser, fromChan, DateTime.Now, String.Format("<{0}> {1}", fromNick, message)})
         tellHandle.Save()
         sendMessage(fromChan, String.Format("{0}: I'll pass that along.", fromNick))
     End Sub
@@ -40,7 +40,7 @@ Module tellHandle
             If i < waitingTellsList.Count Then
                 If nickname.ToLower() = waitingTellsList(i)(0).ToLower() Then
                     If channel.ToLower() = waitingTellsList(i)(1).ToLower() Then
-                        sendMessage(channel, String.Format("{0}: {1}", nickname, waitingTellsList(i)(2)))
+                        sendMessage(channel, String.Format("{0}: [{1}] {2}", nickname, timeDiff(DateTime.Parse(waitingTellsList(i)(2))), waitingTellsList(i)(3)))
                         waitingTellsList.RemoveAt(i)
                         tellHandle.Save()
                         i = i - 1
@@ -49,4 +49,12 @@ Module tellHandle
             End If
         Next
     End Sub
+    Function timeDiff(time As DateTime)
+        Dim ts As TimeSpan = DateTime.Now - time
+        If ts.TotalDays > 1 Then Return String.Format("{0} days, {1}:{2}:{3} ago", ts.Days, ts.Hours.ToString().PadLeft(2, "0"), ts.Minutes.ToString().PadLeft(2, "0"), ts.Seconds.ToString().PadLeft(2, "0"))
+        If ts.TotalHours > 1 Then Return String.Format("{0}:{1}:{2} ago", ts.Hours.ToString().PadLeft(2, "0"), ts.Minutes.ToString().PadLeft(2, "0"), ts.Seconds.ToString().PadLeft(2, "0"))
+        If ts.TotalMinutes > 1 Then Return String.Format("{0} minutes, {1} seconds ago", ts.Minutes, ts.Seconds)
+        If ts.TotalSeconds > 1 Then Return String.Format("{0} seconds ago", ts.Seconds)
+        Return Nothing
+    End Function
 End Module
